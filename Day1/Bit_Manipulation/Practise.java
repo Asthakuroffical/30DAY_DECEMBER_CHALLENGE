d4bb8709fe63c119fcdf0a229a30a744bda2118f
Abhishek Singh <122505940+Asthakuroffical@users.noreply.github.com>
2025-12-02 23:56:34 +0530

    Update Practise.java
    Added a main method to demonstrate bit manipulation.

--- Day1/Bit_Manipulation/Practise.java
+++ Day1/Bit_Manipulation/Practise.java
@@ -1,3 +1,7 @@
 public class Practise {
-    
+    public static void main(String[] args){
+        //What is the value of x^x for any valueof x
+        int x = 3;
+        System.out.print(x^x);
+    }                        
 }
